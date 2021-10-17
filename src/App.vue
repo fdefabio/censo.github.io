2b4aac0da0b939242f35f6d488d74276b91e7658
fabio andres balanta <fabioannypro47@gmail.com>
2021-10-16 19:58:09 -0500

    navbar

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,25 @@
   <div id="nav">
     <router-link to="/" id="linknav">Registros</router-link> |
     <router-link to="/about" id="linknav">Agregar</router-link>|
-    <router-link to="" id="linknav">Estadisticas</router-link>|
-    <a id="buttonreg" href="#" class="button">registrarse</a>
+    <router-link to="" id="linknav">Estadisticas</router-link>
+
+    <div class="registrarbutton">
+      <a id="buttonreg" href="#" class="button">Ingresar</a>
+    </div>
+
+    <div class="title">
+      <h2>Censo Indigena</h2>
+    </div>
   </div>
 
   <!-- <router-view />-->
 </template>
 
 <style>
+* {
+  /*border: 1px solid;*/
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,19 +30,21 @@
 }
 
 #nav {
-  padding: 30px;
+  padding: 15px;
   background-color: rgb(127, 157, 255);
+  width: 100%;
 }
 
-#nav a {
+#nav {
   font-weight: bold;
   color: #2c3e50;
+  height: 30px;
 }
 
 #nav #linknav {
   position: relative;
   color: #f4f7fa;
-  padding: 10px;
+  padding: 28px;
   letter-spacing: 1px;
   text-decoration: none;
 }
@@ -43,7 +56,7 @@
   height: 3px;
   width: 0%;
   left: 0;
-  bottom: -2px;
+  bottom: 8px;
   transition: 0.5s;
 }
 
@@ -52,7 +65,6 @@
 }
 
 #buttonreg {
-  display: inline-block;
   color: #f4f7fa;
   background-color: rgb(127, 157, 255);
   border: 2px solid;
@@ -61,8 +73,21 @@
   width: 80px;
   padding: 10px;
   margin: 0px;
-  height: 20px;
+  height: 16px;
   text-align: center;
   text-decoration: none;
+}
+
+.registrarbutton {
+  width: 10%;
+  margin-left: 1500px;
+  margin-top: -8px;
+}
+
+.title {
+  width: 500px;
+  margin-top: -45px;
+  padding-top: 0px;
+  color: #f4f7fa;
 }
 </style>
---
+++ src/components/tablaRegistros.vue
@@ -0,0 +1,15 @@
+<template>
+  <form action="">
+    <input type="password" />
+  </form>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<style></style>
